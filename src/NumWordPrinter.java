f0586b4cabd2d901f5786ad76b5905433e90ac99
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-09-24 12:43:45 +0600

    Added numberWordPrinter class that includes the algorithms

---
+++ src/NumWordPrinter.java
@@ -0,0 +1,30 @@
+public class NumWordPrinter {
+
+    public String printWord(int num, boolean uppercase) {
+        String result;
+
+
+        if (num % 2 == 0 && num % 11 == 0 && num % 13 == 0) {
+            result = "EvenPrimeLucky";
+        } else if (num % 2 == 0 && num % 11 == 0) {
+            result = "EvenPrimeEleven";
+        } else if (num % 2 == 0) {
+            result = "Even";
+        } else if (num % 11 == 0) {
+            result = "PrimeEleven";
+        } else if (num % 13 == 0) {
+            result = "LuckyThirteen";
+        } else {
+            result = "OddOneOut";
+        }
+
+
+        if (uppercase) {
+            result = result.toUpperCase();
+        }
+
+        return result;
+    }
+
+}
+
---
+++ src/NumWordPrinterTest.java
@@ -0,0 +1,2 @@
+public class NumWordPrinterTest {
+}
