36f9a7c1d211d713e5af99027b5cab0164cc73e3
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-09-24 12:49:01 +0600

    added one test case of divisibility by 2

--- src/NumWordPrinterTest.java
+++ src/NumWordPrinterTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class NumWordPrinterTest {
+
+    NumWordPrinter printer = new NumWordPrinter();
+
+
+    @Test
+    public void testDivisibleBy2() {
+        assertEquals("Even", printer.printWord(4, false));
+    }
+
 }
+
